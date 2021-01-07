fb10f5b64c2c67dcd621b7f0f40c3274037033a8
sureshkumabj <sureshkumarbj@gmail.com>
2021-01-07 17:00:53 +0530

    Sub.php Subject variable included

--- subs.php
+++ subs.php
@@ -30,13 +30,14 @@
 <?php
 
 $email = $_POST['email'];
+$subject = $_POST['title'];
 
 
 $formcontent=" From: $email requested for Newsletter";
 $recipient = "sales@spizeup.in";
 
 $mailheader = "From: $email \r\n";
-mail($recipient, $formcontent, $mailheader) or die("Error!");
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 // echo "thankyou.html";
 header('Location: thankyou-subs.html');
 exit();
